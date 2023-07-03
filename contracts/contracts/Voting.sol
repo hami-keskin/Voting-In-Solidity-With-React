9ee3f8d89f8e1c19a16f75f22ae13d72a27ad233
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-07-03 17:38:20 +0300

    Update Voting.sol

--- contracts/contracts/Voting.sol
+++ contracts/contracts/Voting.sol
@@ -21,11 +21,7 @@ contract Voting {
 
     function vote(bool _vote) public {
         require(!voteCompleted, "Voting has been completed.");
-        require(authenticate(), "Authentication failed.");
-        require(
-            block.timestamp < votingDeadline,
-            "Voting deadline has passed."
-        );
+        require(block.timestamp < votingDeadline, "Voting deadline has passed.");
         require(!voterVoted[msg.sender], "This voter has already voted.");
 
         if (_vote) {
@@ -40,13 +36,6 @@ contract Voting {
         emit VoteCasted(msg.sender, _vote);
     }
 
-    function authenticate() internal pure returns (bool) {
-        // A real authentication process should be implemented here.
-        // For example, you can communicate with an external authentication service.
-        // In this example, it is assumed that every user is authenticated and true is returned.
-        return true;
-    }
-
     function startVoting(uint256 _duration) public {
         require(msg.sender == contractOwner, "Not the contract owner.");
         require(voteCompleted, "Voting has not been completed yet.");
@@ -56,6 +45,8 @@ contract Voting {
         delete voters;
         votingDeadline = block.timestamp + _duration;
         voteCompleted = false;
+
+        emit VotingReset(votingDeadline);
     }
 
     function voteResults()
@@ -90,6 +81,7 @@ contract Voting {
     function checkVotingCompletion() public {
         if (block.timestamp > votingDeadline) {
             voteCompleted = true;
+            emit VotingCompleted();
         }
     }
 }
