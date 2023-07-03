f265add6cbc1ff8ab4bfe802b187cfb7d45c23f9
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-07-03 18:00:53 +0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import {
   ThirdwebProvider,
   useContract,
   useContractRead,
+  useContractWrite,
 } from "@thirdweb-dev/react";
 
 function App() {
@@ -16,10 +17,10 @@ function App() {
 
 function Component() {
   const contractAddress = "0x06Eb9F83DF91113627875D07De6b60676Ceb4102";
-
   const { contract, isLoading: contractLoading } = useContract(contractAddress);
 
   const [contractReady, setContractReady] = useState(false);
+  const [duration, setDuration] = useState(0);
 
   useEffect(() => {
     if (contract) {
@@ -27,10 +28,34 @@ function Component() {
     }
   }, [contract]);
 
-  const { data: contractOwner } = useContractRead(contract, "contractOwner", []);
-  const { data: getRemainingDeadline } = useContractRead(contract, "getRemainingDeadline", []);
-  const { data: voteCompleted } = useContractRead(contract, "voteCompleted", []);
+  const { data: contractOwner } = useContractRead(
+    contract,
+    "contractOwner",
+    []
+  );
+  const { data: getRemainingDeadline } = useContractRead(
+    contract,
+    "getRemainingDeadline",
+    []
+  );
+  const { data: voteCompleted } = useContractRead(
+    contract,
+    "voteCompleted",
+    []
+  );
   const { data: voteResults } = useContractRead(contract, "voteResults", []);
+
+  const { mutateAsync: startVoting, isLoading: startVotingLoading } =
+    useContractWrite(contract, "startVoting");
+
+  const handleStartVoting = async () => {
+    try {
+      const data = await startVoting({ args: [duration] });
+      console.info("Contract call success:", data);
+    } catch (error) {
+      console.error("Contract call failed:", error);
+    }
+  };
 
   return (
     <div>
@@ -40,9 +65,24 @@ function Component() {
         <>
           <h1>Voting Contract</h1>
           <p>Contract Owner: {contractOwner && contractOwner.toString()}</p>
-          <p>Get Voting Deadline: {getRemainingDeadline && getRemainingDeadline.toString()}</p>
+          <p>
+            Get Voting Deadline:{" "}
+            {getRemainingDeadline && getRemainingDeadline.toString()}
+          </p>
           <p>Vote Completed: {voteCompleted && voteCompleted.toString()}</p>
           <p>Vote Results: {voteResults && voteResults.toString()}</p>
+          <div>
+            <label htmlFor="duration">Voting Duration:</label>
+            <input
+              type="number"
+              id="duration"
+              value={duration}
+              onChange={(e) => setDuration(e.target.value)}
+            />
+            <button onClick={handleStartVoting} disabled={startVotingLoading}>
+              Start Voting
+            </button>
+          </div>
         </>
       ) : (
         <p>Contract not found.</p>
