44f69ff098b0d137b86e94ae142250756345d34b
Hami Keskin <hami.keskin@bil.omu.edu.tr>
2023-07-03 23:11:30 +0300

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -16,7 +16,7 @@ function App() {
 }
 
 function Component() {
-  const contractAddress = "0xEcd8158587988bb9975e4e69a65bBAA76B3b9C26";
+  const contractAddress = "0xd1de7abe3851bf918169dacbC6234D63Ac5cD2de";
   const { contract, isLoading: contractLoading } = useContract(contractAddress);
 
   const [contractReady, setContractReady] = useState(false);
@@ -27,31 +27,49 @@ function Component() {
     }
   }, [contract]);
 
-  const { data: contractOwner } = useContractRead(
-    contract,
-    "contractOwner",
-    []
-  );
-  const { data: getRemainingDeadline } = useContractRead(
-    contract,
-    "getRemainingDeadline",
-    []
-  );
+  const { data: contractOwner } = useContractRead(contract, "contractOwner", []);
+  const { data: getRemainingDeadline } = useContractRead(contract, "getRemainingDeadline", []);
   const { data: voteResults } = useContractRead(contract, "voteResults", []);
 
-  const { mutateAsync: startVoting, isLoading } = useContractWrite(
+  const { mutateAsync: startVoting, isLoading: startVotingLoading } = useContractWrite(
     contract,
     "startVoting"
   );
 
+  const { mutateAsync: endVoting, isLoading: endVotingLoading } = useContractWrite(
+    contract,
+    "endVoting"
+  );
+
+  const { mutateAsync: vote, isLoading: voteLoading } = useContractWrite(contract, "vote");
+
   const [duration, setDuration] = useState(20);
+  const [voteValue, setVoteValue] = useState(0);
 
   const handleStartVoting = async () => {
     try {
       const data = await startVoting({ args: [duration] });
-      console.info("Contract call success", data);
+      console.info("Start Voting contract call success", data);
     } catch (err) {
-      console.error("Contract call failure", err);
+      console.error("Start Voting contract call failure", err);
+    }
+  };
+
+  const handleEndVoting = async () => {
+    try {
+      const data = await endVoting();
+      console.info("contract call success", data);
+    } catch (err) {
+      console.error("contract call failure", err);
+    }
+  };
+
+  const handleVote = async () => {
+    try {
+      const data = await vote({ args: [voteValue] });
+      console.info("Vote contract call success", data);
+    } catch (err) {
+      console.error("Vote contract call failure", err);
     }
   };
 
@@ -76,8 +94,26 @@ function Component() {
               onChange={(e) => setDuration(Number(e.target.value))}
             />
           </label>
-          <button onClick={handleStartVoting} disabled={isLoading}>
-            Start Voting
+          <button onClick={handleStartVoting} disabled={startVotingLoading}>
+            {startVotingLoading ? "Starting Voting..." : "Start Voting"}
+          </button>
+          <br />
+          <br />
+          <label>
+            Vote Value:
+            <input
+              type="number"
+              value={voteValue}
+              onChange={(e) => setVoteValue(Number(e.target.value))}
+            />
+          </label>
+          <button onClick={handleVote} disabled={voteLoading}>
+            {voteLoading ? "Voting..." : "Vote"}
+          </button>
+          <br />
+          <br />
+          <button onClick={handleEndVoting} disabled={endVotingLoading}>
+            {endVotingLoading ? "Ending Voting..." : "End Voting"}
           </button>
         </>
       ) : (
